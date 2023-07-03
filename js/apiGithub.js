b3454f6d4e05c02615cff4d313edc007ee20e314
Thiago Romulo <63716510+ThiagoRomulo@users.noreply.github.com>
2023-07-03 09:49:30 -0300

    Add files via upload

---
+++ js/apiGithub.js
@@ -0,0 +1,15 @@
+// Comunicação com a API
+export class CommAPI {
+  static search(username) {
+    const endpoint = `https://api.github.com/users/${username}`
+
+    return fetch(endpoint)
+      .then((data) => data.json())
+      .then(({ login, name, public_repos, followers }) => ({
+        login,
+        name,
+        public_repos,
+        followers,
+      }))
+  }
+}
